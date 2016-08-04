9ee2482853c0f939f0649a1747286c4f6fe87307
Norbert Kuemin <momo_102@bluemail.ch>
2016-08-04 13:29:44 +0200

    Fix warnings New version

--- mod_cm_birthday.php
+++ mod_cm_birthday.php
@@ -11,7 +11,7 @@
 defined( '_JEXEC' ) or die;
 
 function getBirthdayData($fields, $where, $order) {
-	$fields[0] = "DISTINCT ".$fields[0]; //Ugly hack to eliminate duplicates
+	$fields[0] = 'DISTINCT '.$fields[0]; //Ugly hack to eliminate duplicates
 	$db = JFactory::getDBO();
 	$query = $db->getQuery(true);
 	$query->select($fields)
@@ -24,7 +24,7 @@ function getBirthdayData($fields, $where, $order) {
 	return $data;
 }
 
-function displayBirthdays($type, $items, $details, $cols, $colcount, $params, $cmparams, $uri, $bdtext="") {
+function displayBirthdays($type, $items, $details, $cols, $colcount, $params, $cmparams, $uri, $bdtext='') {
 	$name = array();
 	$collink = $params->get('detail_column_link');
 	$coloffset  = 4;
@@ -39,141 +39,135 @@ function displayBirthdays($type, $items, $details, $cols, $colcount, $params, $c
 		}
 		for($j=0;$j<$colcount;$j++) {
 			if ($details && ($collink == $cols[$j])) {
-				$row[$j] = "<a href=\"".$uri->toString()."\" class=\"modal\" rel=\"{handler: 'iframe', size: {x: ".$cmparams->get( 'detail_width' ).", y: ".$cmparams->get( 'detail_height' )."}}\">".$row[$j]."</a>";
+				$row[$j] = '<a href="'.$uri->toString().'" class="modal" rel="{handler: \'iframe\', size: {x: '.$cmparams->get('detail_width').', y: '.$cmparams->get('detail_height').'}}">'.$row[$j].'</a>';
 			}
 		}
-		$agetext = "";
-		if ($params->get( 'column_next_age' ) == "1") {
-			$age = JHTML::_('date', $birthday, "Y") - JHTML::_('date', $birthdate, "Y");
+		$agetext = '';
+		if ($params->get('column_next_age') == '1') {
+			$age = JHTML::_('date', $birthday, 'Y') - JHTML::_('date', $birthdate, 'Y');
 			$agetext = ", ".$age." ".JText::_('MOD_CM_BIRTHDAY_FIELD_UPCOMMING_AGE_YEARS');
 		}
-		if ($bdtext != "") {
-			$birthday = " <span class=\"cmbirth_".$type."_person_birthday\">(".$bdtext.$agetext.")</span>";
+		if (!empty($bdtext)) {
+			$birthday = ' <span class="cmbirth_'.$type.'_person_birthday">('.$bdtext.$agetext.')</span>';
 		} else {
 			switch ($params->get('column_next')) {
-				case "1": //(Weekday)
-					$birthday = " <span class=\"cmbirth_".$type."_person_birthday\">(".JHTML::_('date', $birthday, "D").$agetext.")</span>";
+				case '1': //(Weekday)
+					$birthday = ' <span class="cmbirth_'.$type.'_person_birthday">('.JHTML::_('date', $birthday, 'D').$agetext.')</span>';
 					break;
-				case "2": //(month/day)
-					$birthday = " <span class=\"cmbirth_".$type."_person_birthday\">(".JHTML::_('date', $birthday, "m/d").$agetext.")</span>";
+				case '2': //(month/day)
+					$birthday = ' <span class="cmbirth_'.$type.'_person_birthday">('.JHTML::_('date', $birthday, 'm/d').$agetext.')</span>';
 					break;
-				case "3": //(day.month)
-					$birthday = " <span class=\"cmbirth_".$type."_person_birthday\">(".JHTML::_('date', $birthday, "d.m").$agetext.")</span>";
+				case '3': //(day.month)
+					$birthday = ' <span class="cmbirth_'.$type.'_person_birthday">('.JHTML::_('date', $birthday, 'd.m').$agetext.')</span>';
 					break;
 				default:
-					$birthday = "";
+					$birthday = '';
 					break;
 			}
 		}
-		if ($details && ($params->get( 'detail_column_link' ) == "")) {
-			$name[] = "<a href=\"".$uri->toString()."\" class=\"modal\" rel=\"{handler: 'iframe', size: {x: ".$cmparams->get( 'detail_width' ).", y: ".$cmparams->get( 'detail_height' )."}}\">".implode(" ",$row)."</a>".$birthday;
+		if ($details && ($params->get('detail_column_link') != '')) {
+			$name[] = '<a href="'.$uri->toString().'" class="modal" rel="{handler: \'iframe\', size: {x: '.$cmparams->get('detail_width').", y: ".$cmparams->get('detail_height').'}}">'.implode(' ',$row).'</a>'.$birthday;
 		} else {
-			$name[] = implode(" ",$row).$birthday;
+			$name[] = implode(' ',$row).$birthday;
 		}
 	}
 	return $name;
 }
-
 // Initialize
 jimport('joomla.application.component.model');
 JModelLegacy::addIncludePath(JPATH_SITE.'/components/com_clubmanagement/models');
 $memberModel = JModelLegacy::getInstance( 'Memberships', 'ClubManagementModel' );
-
-$today = date("Y-m-d");
-$cmparams = &JComponentHelper::getParams('com_clubmanagement');
+$EOL = "\n";
+$TAB = "\t";
+$today = date('Y-m-d');
+$cmparams = JComponentHelper::getParams('com_clubmanagement');
 $days = intval($params->get('days_before'));
 $title_today = $params->get('title_today');
 $title_next = $params->get('title_next');
 $details = false;
-if ($params->get('detail_enable') != "0")
-{
+$curi = JFactory::getURI();
+$uri = JURI::getInstance($curi->toString());
+if ($params->get('detail_enable') != '0') {
 	$details = true;
-	$curi =& JFactory::getURI();
-	$uri =& JURI::getInstance($curi->toString());
-	$uri->setVar("layout","detail");
-	$uri->setVar("tmpl","component");
-	$uri->setVar("Itemid","");
-	$uri->setVar("option","com_clubmanagement");
-	$uri->setVar("view","person");
-	$uri->setVar("format","html");
+	$uri->setVar('layout','detail');
+	$uri->setVar('tmpl','component');
+	$uri->setVar('Itemid','');
+	$uri->setVar('option','com_clubmanagement');
+	$uri->setVar('view','person');
+	$uri->setVar('format','html');
 }
-
 // Get columns
 $cols = array();
-for ($i=1;$i<=4;$i++)
-{
+for ($i=1;$i<=4;$i++) {
 	$field = "column_".$i;
-	if ($params->get( $field ) != "")
-	{
+	if ($params->get( $field ) != "") {
 		$cols[] = $params->get($field);
 	}
 }
 $colcount = count($cols);
-$cols[] = "person_id";
-$cols[] = "person_birthday";
-$cols[] = "person_nextbirthday";
+$cols[] = 'person_id';
+$cols[] = 'person_birthday';
+$cols[] = 'person_nextbirthday';
 $fields = $memberModel->translateFieldsToColumns($cols,false);
-
 // Get today records
-$birthdateThisYear = "DATE_ADD(`p`.`birthday`, INTERVAL(YEAR(NOW()) - YEAR(`p`.`birthday`)) YEAR)";
-$birthdateNextYear = "DATE_ADD(`p`.`birthday`, INTERVAL(YEAR(NOW()) - YEAR(`p`.`birthday`) + 1) YEAR)";
-$next_birthday = "IF(".$birthdateThisYear." < CURDATE(), ".$birthdateNextYear.", ".$birthdateThisYear.")";
-$calc_days = "DATEDIFF(".$next_birthday.",'".$today."')";
-$order = $next_birthday." ASC";
-$where_orig = "";
-if ($params->get( 'memberstate' ) == "current") {
-	$where_orig = "`m`.`end` IS NULL OR `m`.`end`='0000-00-00'";
+$birthdateThisYear = 'DATE_ADD(`p`.`birthday`, INTERVAL(YEAR(NOW()) - YEAR(`p`.`birthday`)) YEAR)';
+$birthdateNextYear = 'DATE_ADD(`p`.`birthday`, INTERVAL(YEAR(NOW()) - YEAR(`p`.`birthday`) + 1) YEAR)';
+$next_birthday = 'IF('.$birthdateThisYear.' < CURDATE(), '.$birthdateNextYear.", ".$birthdateThisYear.')';
+$calc_days = 'DATEDIFF('.$next_birthday.',\''.$today.'\')';
+$order = $next_birthday.' ASC';
+$whereList = array();
+if ($params->get('memberstate') == 'current') {
+	$whereList[] = '`m`.`end` IS NULL OR `m`.`end`=\'0000-00-00\'';
 }
-if ($params->get( 'memberstate' ) == "closed") {
-	$where_orig = "`m`.`end` IS NOT NULL AND NOT `m`.`end`='0000-00-00'";
+if ($params->get('memberstate') == 'closed') {
+	$whereList[] = '`m`.`end` IS NOT NULL AND NOT `m`.`end`=\'0000-00-00\'';
 }
-if ($params->get( 'membertype' ) != "") {
-	if ($where_orig != "") { $where_orig .= " AND "; } 
-	$where_orig .= "`m`.`type`='".$params->get( 'membertype' )."'";
+if ($params->get('membertype') != '') {
+	$whereList[] = '`m`.`type`=\''.$params->get('membertype').'\'';
 }
-if ($params->get( 'publicity' ) == "published") {
-	if ($where_orig != "") { $where_orig .= " AND "; } 
-	$where_orig .= "`m`.`published`=1";
+if ($params->get('publicity') == 'published') {
+	$whereList[] = '`m`.`published`=1';
 }
-if ($params->get( 'publicity' ) == "unpublished") {
-	if ($where_orig != "") { $where_orig .= " AND "; } 
-	$where_orig .= "`m`.`published`=0";
+if ($params->get('publicity') == 'unpublished') {
+	$whereList[] = '`m`.`published`=0';
 }
-$where = $where_orig." AND ".$calc_days." = 0";
-
+$whereListToday = $whereList;
+$whereListToday[] = $calc_days.' = 0';
+$where = implode(' AND ',$whereListToday);
 $dataToday = getBirthdayData($fields, $where, $order);
 if ($days > 0) {
-	$where = $where_orig." AND ".$calc_days." BETWEEN 1 AND ".$days;
+	$whereListNext = $whereList;
+	$whereListNext[] = $calc_days.' BETWEEN 1 AND '.$days;
+	$where = implode(' AND ',$whereListNext);
 	$dataNext = getBirthdayData($fields, $where, $order);
 } else {
 	$dataNext = array();
 }
-
 // Display
 if ($details) {
 	JHTML::_('behavior.modal');
 }
-if ($params->get("css") != "") {
-	echo "<style type=\"text/css\" media=\"screen\">\n".$params->get("css")."\n</style>\n";
+if ($params->get('css') != '') {
+	echo '<style type="text/css" media="screen">'.$EOL.$params->get('css').$EOL.'</style>'.$EOL;
 }
-echo "<div class=\"cmbirth\">\n";
+echo '<div class="cmbirth">'.$EOL;
 if (count($dataToday) > 0) {
-	echo "\t<div class=\"cmbirth_today\">\n";
-	if ($title_today != "") {
-		echo "\t\t<div class=\"cmbirth_today_title\">".$title_today."</div>\n";
+	echo $TAB.'<div class="cmbirth_today">'.$EOL;
+	if (!empty($title_today)) {
+		echo $TAB.$TAB.'<div class="cmbirth_today_title">'.$title_today.'</div>'.$EOL;
 	}
-	$name = displayBirthdays("today", $dataToday, $details, $cols, $colcount, $params, $cmparams, $uri, JText::_("Today"));
-	echo "\t\t<div class=\"cmbirth_today_person\">".implode($params->get('delimiter'),$name)."</div>\n";
-	echo "\t</div>\n";
+	$name = displayBirthdays('today', $dataToday, $details, $cols, $colcount, $params, $cmparams, $uri, JText::_('Today'));
+	echo $TAB.$TAB.'<div class="cmbirth_today_person">'.implode($params->get('delimiter'),$name).'</div>'.$EOL;
+	echo $TAB.'</div>'.$EOL;
 }
 if (count($dataNext) > 0) {
-	echo "\t<div class=\"cmbirth_next\">\n";
-	if ($title_next != "") {
-		echo "\t\t<div class=\"cmbirth_next_title\">".$title_next."</div>\n";
+	echo $TAB.'<div class="cmbirth_next">'.$EOL;
+	if (!empty($title_next)) {
+		echo $TAB.$TAB.'<div class="cmbirth_next_title">'.$title_next.'</div>'.$EOL;
 	}
-	$name = displayBirthdays("next", $dataNext, $details, $cols, $colcount, $params, $cmparams, $uri);
-	echo "\t\t<div class=\"cmbirth_next_person\">".implode($params->get('delimiter'),$name)."</div>\n";
-	echo "\t</div>\n";
+	$name = displayBirthdays('next', $dataNext, $details, $cols, $colcount, $params, $cmparams, $uri);
+	echo $TAB.$TAB.'<div class="cmbirth_next_person">'.implode($params->get('delimiter'),$name).'</div>'.$EOL;
+	echo $TAB.'</div>'.$EOL;
 }
-echo "</div>\n";
+echo '</div>'.$EOL;
 ?>
